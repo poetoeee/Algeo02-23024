be881a51ae72bec0496f527b765306e05456cbdc
Julius <juliusarthur2005@gmail.com>
2024-12-07 02:38:28 +0700

    feat: save sliding window at database

--- src/backend/audio/midi_func.py
+++ src/backend/audio/midi_func.py
@@ -1,6 +1,7 @@
 from mido import MidiFile, tempo2bpm
 import numpy as np
 from math import sqrt
+import os
 
 
 #(note, beat_length)
@@ -11,6 +12,12 @@ def get_notes(mid: MidiFile) -> list:
     notes = []
     for i, track in enumerate(mid.tracks):
         if track.name == "Voice":
+            for msg in track:
+                if msg.type == "note_on" and msg.velocity > 0:
+                    notes.append(msg.note)
+            return notes
+        
+        elif track.name == "Lead":
             for msg in track:
                 if msg.type == "note_on" and msg.velocity > 0:
                     notes.append(msg.note)
@@ -48,7 +55,6 @@ def atb_hist_time_measure(l: list):
 
 def rtb_hist_count_measure(notes: list):
     '''get histogram of tone difference of each succesive note'''
-    
     rtb_notes = []
 
     for i in range(len(notes) - 1):
@@ -114,7 +120,7 @@ def hist_normalize(notes: list):
         sum += note
 
     for note in notes:
-        normal.append((note/sum).item())
+        normal.append((note/sum))
 
     return normal
 
@@ -131,7 +137,6 @@ def cos_vector(l1: list, l2: list):
 
     norm_l1 = sqrt(norm_l1)
     norm_l2 = sqrt(norm_l2)
-
     return dot_product/(norm_l1 * norm_l2)
 
 def divide_to_beat(midi: MidiFile) -> list:
@@ -148,10 +153,24 @@ def divide_to_beat(midi: MidiFile) -> list:
                     if msg.time > 0:
                         beat_count = msg.time / tpb #msg.time dihitung dalam tick
                         res.append((msg.note, beat_count))
+            return res
+        
+        elif track.name == "Lead":
+            for msg in track:
+                if msg.type == "note_on" or msg.type == "note_off":
+                    if msg.time > 0:
+                        beat_count = msg.time / tpb #msg.time dihitung dalam tick
+                        res.append((msg.note, beat_count))
+            return res
+        
     return res
 
 def divide_to_segment(events: list):
-    '''Membagi list menjadi beberapa list yang masing masing segmen terdiri dari 20 - 40 beat'''
+    '''Membagi list menjadi beberapa list yang masing masing segmen terdiri dari 20 - 40 beat
+    events: list of tuple (note, beat)
+
+    Menghasilkan list of list of tuple (note, beat)
+    '''
 
     res = []
     temp = []
@@ -165,7 +184,8 @@ def divide_to_segment(events: list):
             res.append(temp)
             temp = []
             beat_count = 0
-        
+    if beat_count != 0:
+        res.append(temp)
     return res
 
 def compare(h1, h2):
@@ -180,12 +200,198 @@ def compare(h1, h2):
     cos_res = cos_vector(h1, h2)
     return cos_res
 
-def calculate(mid1: MidiFile, mid2: MidiFile):
-    '''
-    Membandingkan 2 file midi, mid1 sebagai humming, dan mid2 dari database.
-    '''
 
+def save_numpy_file():
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    directory_in_str = os.path.join(current_directory, "database_song", "midi_dataset")
+    print(directory_in_str)
+    database = {}
+
+    window_size = 4
+    for file in os.listdir(directory_in_str):
+        filename = os.path.join(directory_in_str, file)
+        mid = MidiFile(filename)
+        
+        events = divide_to_beat(mid)
+        segments = divide_to_segment(events)
+
+        windows = []
+        for segment in segments:
+            beat_count = 0
+            window = []
+            i = 0
+            while i < len(segment):
+                event = segment[i]
+
+                n, b = event
+                if beat_count + b < window_size:
+                    window.append(event)
+                    beat_count += b
+                    i += 1
+                else:
+                    windows.append(window[:])
+                    if window != []:
+                        (a, c) = window[0]
+                        window.pop(0)
+                        beat_count -= c
+                    else:
+                        window.append(event)
+                        beat_count  += b
+                        i+= 1
+
+                    
+            if window != []:
+                windows.append(window[:])
+        
+        database[file] = windows
+
+    f = os.path.join(current_directory, "np_database")
+    np.save(f, database)
+    print("database saved")
+
+
+
+
+
+def calculate_time_measure(mid1: MidiFile, mid2: MidiFile, feature: int):
+    '''
+    Membandingkan 2 list of tuple (note, beat).
+    feature = 1: atb
+    feature = 2: rtb
+    feature = 3: ftb
+
+    '''
     event1 = divide_to_beat(mid1)
     event2 = divide_to_beat(mid2)
-    
+    if event1 == [] or event2 == []:
+        return -1
+    segments1 = divide_to_segment(event1)
+    segments2 = divide_to_segment(event2)
 
+
+    res = -1
+    window1 = []
+    window2 = []
+    window_size = 8
+    for segment1 in segments1:
+        for segment2 in segments2:
+            for i in range(len(segment1) - window_size):
+                if window1 == []:
+                    window1 = segment1[:window_size]
+                else:
+                    window1.pop(0)
+                    window1.append(segment1[i + window_size - 1])
+
+                if feature == 0:
+                    h1, b1 = atb_hist_time_measure(window1)
+                elif feature == 1:
+                    h1, b1 = rtb_hist_time_measure(window1)
+                else:
+                    h1, b1 = ftb_hist_time_measure(window1)
+
+                h1 = hist_normalize(h1)
+                window2 = []
+
+                for j in range(len(segment2) - window_size):                    
+                    if window2 == []:
+                        window2 = segment2[:window_size]
+                    else:
+                        window2.pop(0)
+                        window2.append(segment2[j + window_size - 1])
+
+                    if feature == 0:
+                        h2, b2 = atb_hist_time_measure(window2)
+                        
+                    elif feature == 1:
+                        h2, b2 = rtb_hist_time_measure(window2)
+
+                    else:
+                        h2, b2 = ftb_hist_time_measure(window2)
+
+                    h2 = hist_normalize(h2)
+                    
+
+                    kemipiran = compare(h1, h2)
+                    # print("kemiripan: ", kemipiran)
+
+                    if res < kemipiran.item():
+                        res = kemipiran.item()
+
+                    if kemipiran > 0.9:
+                        return res
+    return res
+
+
+    
+def calculate_count_measure(mid1: MidiFile, mid2: MidiFile, feature: int):
+    '''
+    Membandingkan 2 list of tuple (note, beat).
+    feature = 1: atb
+    feature = 2: rtb
+    feature = 3: ftb
+
+    '''
+    event1 = divide_to_beat(mid1)
+    event2 = divide_to_beat(mid2)
+    if event1 == [] or event2 == []:
+        return -1
+
+    segments1 = divide_to_segment(event1)
+    segments2 = divide_to_segment(event2)
+
+
+    res = -1
+    window1 = []
+    window2 = []
+    window_size = 8
+    for segment1 in segments1:
+        for segment2 in segments2:
+            for i in range(len(segment1) - window_size):
+                if window1 == []:
+                    window1 = segment1[:window_size]
+                    window1 = [a for a, b in window1]
+                else:
+                    window1.pop(0)
+                    a, b = segment1[i + window_size - 1]
+                    window1.append(a)
+
+                if feature == 0:
+                    h1, b1 = atb_hist_count_measure(window1)
+                elif feature == 1:
+                    h1, b1 = rtb_hist_count_measure(window1)
+                else:
+                    h1, b1 = ftb_hist_count_measure(window1)
+
+                h1 = hist_normalize(h1)
+                window2 = []
+
+                for j in range(len(segment2) - window_size):                    
+                    if window2 == []:
+                        window2 = segment2[:window_size]
+                        window2 = [a for a, b in window2]
+                    else:
+                        window2.pop(0)
+                        a, b = segment2[j + window_size - 1]
+                        window2.append(a)
+
+                    if feature == 0:
+                        h2, b2 = atb_hist_count_measure(window2)
+
+                    elif feature == 1:
+                        h2, b2 = rtb_hist_count_measure(window2)
+
+                    else:
+                        h2, b2 = ftb_hist_count_measure(window2)
+
+                    h2 = hist_normalize(h2)
+                    
+
+                    kemipiran = compare(h1, h2)
+                    # print("kemiripan: ", kemipiran)
+
+                    if kemipiran.item() > res:
+                        res = kemipiran.item()
+
+                    if kemipiran > 0.9:
+                        return res
+    return res
