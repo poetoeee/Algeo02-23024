6e6317f8b1bc6c1cf511a43e7e0e9cbfbb16548e
Julius <juliusarthur2005@gmail.com>
2024-12-09 12:55:32 +0700

    feat:main

--- src/backend/audio/main.py
+++ src/backend/audio/main.py
@@ -3,13 +3,47 @@ import numpy as np
 from midi_func import save_numpy_file
 from midi_func import divide_to_beat
 import os
+import mido
+mid1 = mido.MidiFile(r"C:\Users\User\Algeo2\Algeo02-23024\src\backend\audio\database_song\midi_dataset\x (1).mid")
 
 
-save_numpy_file()
+segments = mf.divide_to_segment(divide_to_beat(mid1))
+
+save_numpy_file()#create np file of window
 d = np.load(r"C:\Users\User\Algeo2\Algeo02-23024\src\backend\audio\window_database.npy", allow_pickle=True).item()
 keys = list(d.keys())
-# print(keys[29])
-# print(d["x (35).mid"])
-#     print(i)
-for i in d[keys[29]]:
-    print(i)
+
+for key in keys:
+    res = 0
+    res_all = []
+    for window in d[key]:
+        if window != []:
+
+            for segment in segments:
+                h1, b1 = mf.atb_hist_time_measure(segment)
+                h2, b2 = mf.atb_hist_time_measure(window)
+
+                res+= mf.compare(h1, h2)
+
+                h1, b1 = mf.rtb_hist_time_measure(segment)
+                h2, b2 = mf.rtb_hist_time_measure(window)
+
+                res+= mf.compare(h1, h2)
+
+                h1, b1 = mf.ftb_hist_time_measure(segment)
+                h2, b2 = mf.ftb_hist_time_measure(window)
+
+                res+= mf.compare(h1, h2)
+                
+                res /= 3
+                # print("res",)
+
+
+                res_all.append(res)
+
+    print(f"hasil dengan {key}:",  np.average(res_all))
+
+        
+
+
+
