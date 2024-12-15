8ab287b1349bdf4526197f885905c967f54520b0
Julius <juliusarthur2005@gmail.com>
2024-12-15 17:10:42 +0700

    fix:upload image api

--- src/backend/server.py
+++ src/backend/server.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import os
 import zipfile
 from audio.main import handle_query_audio, procces_audio_db
-# from image.integration import handle_query, process_dataset
+from image.integration import handle_query, process_dataset
 
 #app instance
 app = Flask(__name__)
@@ -33,6 +33,8 @@ def compare_song():
 @app.route("/api/upload_song", methods= ["POST"])
 def procces_song():
     try:
+        temp = os.path.join(r"src\backend\audio\temp")
+        os.makedirs(temp, exist_ok=True)
        
         uploaded_file = request.files.get('file_song_db')
 
@@ -68,13 +70,31 @@ def procces_song():
 #         return jsonify({'error' : str(e)}), 500
 
 
-# @app.route("/api/upload_image", methods= ["GET"])
-# def procces_image():
-#     try:
-#         process_dataset("database_image", "processed_data.npy", )
-#         return jsonify({"message": "Image database processed successfully"})
-#     except Exception as e:
-#         return jsonify({'error' : str(e)}), 500
+@app.route("/api/upload_image", methods= ["POST"])
+def procces_image():
+    try:
+        temp = os.path.join(r"src\backend\image\temp")
+        os.makedirs(temp, exist_ok=True)
+        uploaded_file = request.files.get('file_image_db')
+
+        if not uploaded_file:
+            return jsonify({'error': 'No file uploaded'}), 400
+        
+        zip_path = os.path.join(r"src\backend\image\temp", uploaded_file.filename)
+        uploaded_file.save(zip_path)
+
+        extracted = os.path.join(r"src\backend\image\db_tes")
+        os.makedirs(extracted, exist_ok=True)
+
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(extracted)
+
+        process_dataset(extracted, "processed_data.npy")
+
+        return jsonify({"message": "Image database processed successfully"})
+    
+    except Exception as e:
+        return jsonify({'errornya' : str(e)}), 500
     
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
