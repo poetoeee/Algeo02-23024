3732a6895fa45df67b778ffd8583f0765b1acce6
Julius <juliusarthur2005@gmail.com>
2024-12-15 15:36:16 +0700

    feat:api for song and image proces

--- src/backend/server.py
+++ src/backend/server.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from audio.main import handle_query_audio, procces_audio_db
+from image.integration import handle_query, process_dataset
 
 #app instance
 app = Flask(__name__)
@@ -12,6 +14,36 @@ def return_home():
     })
 
 
+@app.route("/api/compare_song", methods = ['POST'])
+def compare_song():
+    try:
+        res = handle_query_audio()
+        return jsonify(res)
+    except Exception as e:
+        return jsonify({'error' : str(e)}), 500
 
+@app.route("/api/upload_song", methods = ['POST'])
+def procces_song():
+    try:
+        procces_audio_db()
+        return 1
+    except Exception as e:
+        return jsonify({'error' : str(e)}), 500
+
+@app.route("api/compare_image", methods = ["POST"])
+def compare_song():
+    try:
+        res = handle_query("image_query.jpg")
+        return jsonify(res)
+    except Exception as e:
+        return jsonify({'error' : str(e)}), 500
+
+@app.route("api/upload_image", methods = [])
+def procces_image():
+    try:
+        process_dataset("database_image", "", )
+        return 1
+    except Exception as e:
+        return jsonify({'error' : str(e)}), 500
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
