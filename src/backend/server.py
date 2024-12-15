c46fc6d008980da27e4c47ef2433bcfe752e7a58
Julius <juliusarthur2005@gmail.com>
2024-12-15 16:44:27 +0700

    feat:zip path structuer

--- src/backend/server.py
+++ src/backend/server.py
@@ -1,7 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import os
+import zipfile
 from audio.main import handle_query_audio, procces_audio_db
-from image.integration import handle_query, process_dataset
+# from image.integration import handle_query, process_dataset
 
 #app instance
 app = Flask(__name__)
@@ -17,33 +19,62 @@ def return_home():
 @app.route("/api/compare_song", methods = ['POST'])
 def compare_song():
     try:
-        res = handle_query_audio()
+
+        query = request.files.get('file_song')
+        res = handle_query_audio(query.filename)
+
+        if not query:
+            return jsonify({'error': 'No file uploaded'}), 400
         return jsonify(res)
+    
     except Exception as e:
         return jsonify({'error' : str(e)}), 500
 
-@app.route("/api/upload_song", methods = ['POST'])
+@app.route("/api/upload_song", methods= ["POST"])
 def procces_song():
     try:
-        procces_audio_db()
-        return 1
+       
+        uploaded_file = request.files.get('file_song_db')
+
+        if not uploaded_file:
+            return jsonify({'error': 'No file uploaded'}), 400
+        
+        zip_path = os.path.join(r"src\backend\audio\temp", uploaded_file.filename)
+        uploaded_file.save(zip_path)
+
+        extracted = os.path.join(r"src\backend\audio\database_song")
+        os.makedirs(extracted, exist_ok=True)
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(extracted)
+
+        procces_audio_db(path=extracted)
+
+        return jsonify({"message": "Song database processed successfully"})
     except Exception as e:
         return jsonify({'error' : str(e)}), 500
 
-@app.route("api/compare_image", methods = ["POST"])
-def compare_song():
-    try:
-        res = handle_query("image_query.jpg")
-        return jsonify(res)
-    except Exception as e:
-        return jsonify({'error' : str(e)}), 500
+# @app.route("/api/compare_image", methods = ["POST"])
+# def compare_image():
+#     try:
+#         query = request.files.get('file_image')
+#         res = handle_query(query.filename)
 
-@app.route("api/upload_image", methods = [])
-def procces_image():
-    try:
-        process_dataset("database_image", "", )
-        return 1
-    except Exception as e:
-        return jsonify({'error' : str(e)}), 500
+#         if not query:
+#             return jsonify({'error': 'No file uploaded'}), 400
+        
+#         return jsonify(res)
+    
+#     except Exception as e:
+#         return jsonify({'error' : str(e)}), 500
+
+
+# @app.route("/api/upload_image", methods= ["GET"])
+# def procces_image():
+#     try:
+#         process_dataset("database_image", "processed_data.npy", )
+#         return jsonify({"message": "Image database processed successfully"})
+#     except Exception as e:
+#         return jsonify({'error' : str(e)}), 500
+    
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
