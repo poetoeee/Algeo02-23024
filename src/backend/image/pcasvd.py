fdae2db4fd43f69a2a01b505e800662bccab0827
Julius <juliusarthur2005@gmail.com>
2024-12-15 17:37:32 +0700

    fix:assign k value automaticly

--- src/backend/image/pcasvd.py
+++ src/backend/image/pcasvd.py
@@ -1,7 +1,12 @@
 import numpy as np
 from scipy.sparse.linalg import svds
 
-def compute_svd(data, k): #data - normalized data, k yang dikeep
+def compute_svd(data, k=None, fraction = 0.5, max_k = 100): #data - normalized data, k yang dikeep
+
+    num_images = data.shape[0]
+    if k is None:
+        k = max(1, int(fraction * num_images))
+        k = min(k, max_k, num_images)
 
     covariance_matrix = np.dot(data, data.T) / data.shape[1]
 
@@ -15,8 +20,11 @@ def compute_svd(data, k): #data - normalized data, k yang dikeep
     # Transfor eigenvector dari sample ke feature space
     eigenvectors_feature = np.dot(data.T, eigenvectors_sample)
     eigenvectors_feature = eigenvectors_feature / np.linalg.norm(eigenvectors_feature, axis=0)
+    print(f"PCA selesai, k = {k}")
 
     return eigenvectors_feature, singular_values
+
+
 
 def project_data(data, eigenvectors):
     return np.dot(data, eigenvectors)
@@ -30,8 +38,8 @@ def save_data(projected_data, eigenvectors, singular_values):
 
 if __name__ == "__main__":
     normalized_data = np.load("normalized_images.npy")
-    k = 50
-    eigenvectors, singular_values = compute_svd(normalized_data, k)
+    # k = 50
+    eigenvectors, singular_values = compute_svd(normalized_data) #k bisa diubah
     projected_data = project_data(normalized_data, eigenvectors)
 
     save_data(projected_data, eigenvectors, singular_values)
